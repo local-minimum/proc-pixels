f2769c1e4a744813644625c2ef62e129a09eac09
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-01-26 00:20:55 +0100

    Drawing something

--- Assets/Plugins/ProcPixels/Scripts/Artists/NaiveFace.cs
+++ Assets/Plugins/ProcPixels/Scripts/Artists/NaiveFace.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using ProcPixel.Fundamentals;
 using ProcPixel.Palettes;
+using ProcPixel.Utils;
 
 namespace ProcPixel.Artists.Face {
 	
@@ -33,14 +34,16 @@ namespace ProcPixel.Artists.Face {
 				new Vector2(Random.Range(.1f, .3f), Random.Range(.7f, .8f)),
 				new Vector2(Random.Range(.3f, .5f), Random.Range(.8f, .9f)),
 
-				new Vector2(Random.Range(.5f, 7f), Random.Range(.8f, .9f)),
-				new Vector2(Random.Range(.7f, .9f), Random.Range(7f, 8f)),
+				new Vector2(Random.Range(.5f, .7f), Random.Range(.8f, .9f)),
+				new Vector2(Random.Range(.7f, .9f), Random.Range(.7f, .8f)),
 				new Vector2(Random.Range(.7f, 1f), Random.Range(.3f, .5f)),
 				new Vector2(Random.Range(.5f, .8f), Random.Range(0, 0.1f))
 			};
 
 			VerifyRestraints ();
 			ScaleValues ();
+			for (int i=0;i<polygon.Length; i++)
+				Debug.Log (polygon [i]);
 		}
 
 		void VerifyRestraints() {
@@ -55,6 +58,8 @@ namespace ProcPixel.Artists.Face {
 
 		void ScaleValues() {
 			Rect extents = rect;
+			Debug.Log (extents.width);
+			Debug.Log (extents.height);
 			for (int i = 0; i < polygon.Length; i++) {
 				polygon [i].x *=  extents.width;
 				polygon [i].y *= extents.height;
@@ -66,17 +71,10 @@ namespace ProcPixel.Artists.Face {
 			for (int x = 0, X = canvasWidth; x < X; x++) {
 				for (int y = 0, Y = canvasHeight; y < Y; y++) {
 					point = new Vector2 (x, y);
-					if (PointInFeature (point))
+					if (PolygoneMath.PointInPoly(point, polygon))
 						canvas.Draw (x, y, color [ColorShade.Reference]);
 				}
-			}
-
-
+			}				
 		}
-
-		public bool PointInFeature(Vector2 point) {
-			return true;
-		}
-
 	}
 }
---
+++ Assets/Plugins/ProcPixels/Scripts/Utils/PolygoneMath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ProcPixel.Utils {
+	
+	public static class PolygoneMath {
+
+		public static bool PointInPoly(Vector2 point, Vector2[] edges) {
+			Vector2[] transposed = new Vector2[edges.Length];
+			for (int i = 0; i < edges.Length; i++) {
+				transposed [i] = edges [i] - point;
+			}
+			for (int i = 0; i < edges.Length; i++) {
+				int j = (i + 1) % edges.Length;
+				if ((transposed [j].x * transposed [i].y -
+					transposed [i].x * transposed [j].y) <= 0f)
+					return false;
+			}
+			return true;
+		}
+	}
+}
